3e15ef74a307672894ba53de4ccab28b0a742f27
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-06-18 10:02:19 +0100

    Remove travis check in deploy-branch script

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -6,17 +6,25 @@ var path = require('path');
 set('-e');
 set('-v');
 
-var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase();
-if (branch && branch !== 'gh-pages') {
-  var branchPath = path.join('.tmp', 'preview', branch, '/');
-  mkdir('-p', branchPath);
 
-  exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
-  exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
 
-  cp('-R', 'output/*', branchPath);
-  rm('-rf', 'output')
+let branch = exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+    if (err) {
+      // handle your error
+    }
+    return stdout;
+  });
 
-  cp('-R', 'web/*', branchPath);
-  exec('deploy-to-gh-pages --update .tmp');
-}
+
+var branchPath = path.join('.tmp', 'preview', branch, '/');
+mkdir('-p', branchPath);
+
+exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
+exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
+
+cp('-R', 'output/*', branchPath);
+rm('-rf', 'output')
+
+cp('-R', 'web/*', branchPath);
+exec('deploy-to-gh-pages --update .tmp');
+
