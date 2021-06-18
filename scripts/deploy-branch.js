cc3ac3b449cc80939e5332d0bf3048fcefb2a613
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-06-18 10:36:23 +0100

    Deploy branch in exec function

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -8,23 +8,25 @@ set('-v');
 
 
 
-let branch = exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+let branch;
+
+exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
     if (err) {
       // handle your error
     }
-    return stdout;
-  });
+    var branchPath = path.join('.tmp', 'preview', stdout, '/');
+    mkdir('-p', branchPath);
+
+    exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
+    exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
+
+    cp('-R', 'output/*', branchPath);
+    rm('-rf', 'output')
+
+    cp('-R', 'web/*', branchPath);
+    exec('deploy-to-gh-pages --update .tmp');
+});
 
 
-var branchPath = path.join('.tmp', 'preview', branch, '/');
-mkdir('-p', branchPath);
 
-exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
-exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
-
-cp('-R', 'output/*', branchPath);
-rm('-rf', 'output')
-
-cp('-R', 'web/*', branchPath);
-exec('deploy-to-gh-pages --update .tmp');
 
