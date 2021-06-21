9ec49d47a84f5ce9f2421dcce46f202abaf8d3c0
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-06-21 09:38:10 +0100

    Add github config

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -2,29 +2,31 @@
 'use strict';
 require('shelljs/global');
 var path = require('path');
-var ghpages = require('gh-pages');
 
 set('-e');
 set('-v');
 
 exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
-    if (err) {
-      // handle your error
-    }
-    var branchPath = path.join('.tmp', 'preview', stdout, '/');
-    mkdir('-p', branchPath);
+  if (err) {
+    // handle your error
+  }
+  var branchPath = path.join('.tmp', 'preview', stdout, '/');
+  mkdir('-p', branchPath);
 
-    exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
-    exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
+  exec('dotnet build src/OpenApiGenerator/OpenApiGenerator.csproj');
+  exec('dotnet run -p src/OpenApiGenerator/OpenApiGenerator.csproj');
 
-    cp('-R', 'output/*', branchPath);
-    rm('-rf', 'output')
+  cp('-R', 'output/*', branchPath);
+  rm('-rf', 'output')
 
-    cp('-R', 'web/*', branchPath);
+  cp('-R', 'web/*', branchPath);
 
-    exec(`gh-pages -d .tmp`, (err, stdout, stderr) => {
-    // ...
-    });
+  exec('git config --local user.email "action@github.com"')
+  exec('git config --local user.name "GitHub Action"')
+
+  exec(`gh-pages -d .tmp`, (err, stdout, stderr) => {
+  // ...
   });
+});
   
 
